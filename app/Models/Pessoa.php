ec422850d108e7c773b607ce9d93fafdf25f8ed9
Athyla Guilherme Jeronimo Pires <athylaguilherme20@gmail.com>
2022-10-03 20:18:23 -0300

    primeiro commit

---
+++ app/Models/Pessoa.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Pessoa extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'pessoas';
+    protected $primaryKey = 'id_pessoas';
+    protected $dates = 'dt_nascimento';
+    protected $fillable = ['nome','sobrenome','dt_nascimento','email'];
+}
